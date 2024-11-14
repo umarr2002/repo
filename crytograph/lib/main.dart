7afa028dd11d1d718ad2d660139eef52fc399a34
umarr2002 <umarmukhtarov2002@gmail.com>
2024-11-14 10:40:09 +0500

    first commit

--- crytograph/lib/main.dart
+++ crytograph/lib/main.dart
@@ -13,8 +13,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-      
         //
+        //Change in master!
         // TRY THIS: Try running your application with "flutter run". You'll see
         // the application has a purple toolbar. Then, without quitting the app,
         // try changing the seedColor in the colorScheme below to Colors.green
